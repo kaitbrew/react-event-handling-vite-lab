f0411c018a6fe4e15c0f0f36370289fc938190ae
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2025-07-15 18:33:31 -0400

    submittin that

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,13 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    function handleChange(e){
+        console.log("Entering password...");
+    }
+
     return(
-        <></>
+        <input type="password" onChange={handleChange}
+        placeholder="enter password here"/>
     )
 }
 
